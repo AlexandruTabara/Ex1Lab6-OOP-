d585fe43c08a7cb3f7ecea67fd4f07a95917595a
Alexandru Tabara <alexandru.tab1@gmail.com>
2023-02-01 12:45:24 +0200

    Add project files.

---
+++ Ex1Lab6 (OOP)/Dulap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1Lab6__OOP_
+{
+    public class Dulap
+    {
+        private int lungime;
+        private int latime;
+        private int inaltime;
+
+
+        public Dulap(int lungime, int latime, int inaltime)
+        {
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+        }
+
+        public int CalculeazaVolum()
+        {
+            return lungime * latime * inaltime;
+        }
+    }
+}
+
+
---
+++ Ex1Lab6 (OOP)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex1Lab6__OOP_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //• Ex 1
+            //Un dulap are trei caracteristici: lungime, latime, inaltime.Dulapul va avea o
+            //metoda care va calcula volumul acestuia.
+            //Folositi o clasa care va modela un dulap.
+            // Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+            // Cele trei campuri vor fi initializate cu ajutorul constructorului
+            // Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+            //Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si afisati rezultatul.
+
+            Dulap myDulap = new Dulap(10, 20, 30);
+            int volum = myDulap.CalculeazaVolum();
+
+            Console.WriteLine($"Volumul dulapului este: {volum}");            
+        }
+    }
+}
